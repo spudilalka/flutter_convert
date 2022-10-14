1fe973d80b76c653258c081ff5f6d6c616a63e4b
spudilalka <114853930+spudilalka@users.noreply.github.com>
2022-10-15 01:10:15 +0300

    p

--- lib/bloc/convert_view.dart
+++ lib/bloc/convert_view.dart
@@ -1,8 +1,12 @@
+import 'dart:html';
+
 import 'convert_bloc.dart';
 import 'convert_event.dart';
 import 'convert_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+const List<String> list = <String>['One', 'Two', 'Three', 'Four'];
 
 class ConvertPage extends StatefulWidget {
   const ConvertPage({Key? key}) : super(key: key);
@@ -27,67 +31,109 @@ class _ConvertPageState extends State<ConvertPage> {
 
   double progressBarValue = 0;
 
-  void progressBarUpdate() {
-    progressBarValue = progressBarValue;
+  void progressBarUpdate(progressBarValue) {
+    progressBarValue += 100;
     print("pr bar update");
   }
 
-  List<String> _forTxtAndWordItems = ['PDF', 'Word', 'Txt'];
+  List<String> _forTxtAndWordItems = <String>[
+    'PDF',
+    'Word',
+    'Txt',
+  ];
+
+  String dropdownValue = list.first;
   List<String> _forPNGItems = [
     'Jpeg',
     'BMP',
   ];
+
   @override
   Widget build(BuildContext contex) {
-    return BlocProvider(
-        create: ((context) => ConvertBloc()),
-        child:
-            BlocBuilder<ConvertBloc, ConvertState>(builder: (context, state) {
-          return Container(
-            color: Colors.blueAccent,
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.all(80.0),
-                child: Column(
-                  children: [
-                    DropdownButton(
-                      value: 1,
-                      items: _dropdownItems.map((ListItem item) {
-                        return DropdownMenuItem<int>(
-                          child: Text(item.name),
-                          value: item.value,
-                        );
-                      }).toList(),
-                      onChanged: (format) {
-                        if (format == "PNG") {
-                          PNGformat(format: state.format);
-                        } else if (format == "Word") {
-                          Wordformat(format: state.format);
-                        } else if (format == "Txt") {
-                          Txtformat(format: state.format);
-                        } else {
-                          print('ERROR: GG');
-                        }
-                      },
+    return Scaffold(
+        backgroundColor: Colors.grey[500],
+        appBar: AppBar(
+          title: Text('все фото'),
+          centerTitle: true,
+        ),
+        body: BlocProvider(
+            create: ((context) => ConvertBloc()),
+            child: BlocBuilder<ConvertBloc, ConvertState>(
+                builder: (context, state) {
+              return Container(
+                color: Colors.blueAccent,
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(80.0),
+                    child: Column(
+                      children: [
+                        DropdownButton<String>(
+                          value: dropdownValue,
+                          icon: const Icon(Icons.arrow_downward),
+                          elevation: 16,
+                          style: const TextStyle(color: Colors.deepPurple),
+                          underline: Container(
+                            height: 2,
+                            color: Colors.deepPurpleAccent,
+                          ),
+                          onChanged: (String? value) {
+                            // This is called when the user selects an item.
+                            setState(() {
+                              dropdownValue = value!;
+                            });
+                          },
+                          items: list
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
+
+                        // DropdownButton(
+                        //   value: 1,
+                        //   items: _dropdownItems.map((ListItem item) {
+                        //     return DropdownMenuItem<String>(
+                        //       child: Text(item.name),
+                        //       value: item.format,
+                        //     );
+                        //   },
+
+                        //   ).toList(),
+                        //   onChanged: (value) {
+                        //     if (value == "1") {
+                        //       PNGformat(format: state.format);
+                        //       print('ERROR: 1');
+                        //     } else if (value == "2") {
+                        //       Wordformat(format: state.format);
+                        //     } else if (value == "3") {
+                        //       Txtformat(format: state.format);
+                        //     } else {
+                        //       print('ERROR: GG');
+                        //     }
+                        //   },
+                        // ),
+
+                        GestureDetector(
+                          onTap: (() {
+                            progressBarUpdate(progressBarValue);
+                          }),
+                          child: Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.deepPurple,
+                          ),
+                        ),
+
+                        LinearProgressIndicator(
+                          value: progressBarValue,
+                        ),
+                      ],
                     ),
-                    GestureDetector(
-                      onTap: (() {
-                        progressBarUpdate();
-                      }),
-                      child: Container(
-                        width: 100,
-                        height: 100,
-                        color: Colors.deepPurple,
-                      ),
-                    ),
-                    LinearProgressIndicator(
-                      value: progressBarValue,
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-            ),
-          );
-        }));
+              );
+            })));
   }
 }
